cedf030093f8a71c69f962ba2f64c4c18e1ee154
mahmoudelwekel <mahmoudelwekel@ymail.com>
2020-05-04 20:36:43 +0200

    add navbar

--- StaticGallery/index.js
+++ StaticGallery/index.js
@@ -1,10 +1,25 @@
-﻿var reqCategories = new XMLHttpRequest();
+﻿var cat = getParameterByName('cat');
+
+
+var reqCategories = new XMLHttpRequest();
 reqCategories.open('GET', 'Categories/Categories.json?nocache=' + (new Date()).getTime()); reqCategories.onload = function ()
 {
+    var catnav=document.getElementById("catBtns");
     var Categories = JSON.parse(reqCategories.responseText);
     for (var i = 0; i < Categories.length; i++)
     {
-        document.getElementById("catBtns").insertAdjacentHTML('beforeend', "<a href='index.html?cat=" + i + "' class='btn btn-sm font-weight-bolder btn-outline-primary m-1' role='button' aria-pressed='true'>" + Categories[i].name + "</a>")
+        catnav.insertAdjacentHTML('beforeend', "<a href='Index.html?cat=" + i + "' class='nav-item nav-link  font-weight-bold'>" + Categories[i].name + "</a>  <a class='border'></a>")
+    }
+
+    if (cat != '' && cat != null) {
+        var cate_descrip = document.getElementById('cate_descrip');
+        cate_descrip.insertAdjacentHTML('beforeend', "<div class='alert mt-3 pt-4 alert-info shadow text-right' dir='rtl'><h5 class=' font-weight-bold'> تفاصيل عن القسم :</h5>  " + Categories[cat].description + "</div>");
+
+
+        //$('#catBtns').append($("a[href='Index.html?cat=" + cat + "']"))
+
+        $("a[href='Index.html']").removeClass('active');
+        $("a[href='Index.html?cat=" + cat + "']").addClass('active');
     }
 };
 reqCategories.send();
@@ -34,25 +49,13 @@ function FillProducts(products) {
         document.getElementById("placeinfo").insertAdjacentHTML('beforeend', infolist[0].about);
         $('meta[name=description]').attr('content', infolist[0].about);
 
-        var cat = getParameterByName('cat');
+
 
         if (cat != '' && cat != null)
         {
             var results = [];
             var searchField = "category";
             var searchVal = cat;
-
-            var cate_descrip = document.getElementById('cate_descrip');
-
-            var reqCategories = new XMLHttpRequest();
-            reqCategories.open('GET', 'Categories/Categories.json?nocache=' + (new Date()).getTime());
-            reqCategories.onload = function () {
-                var Categories = JSON.parse(reqCategories.responseText);
-                cate_descrip.insertAdjacentHTML('beforeend', "<div class='alert mt-3 pt-4 alert-info shadow text-right' dir='rtl'><h5 class=' font-weight-bold'> تفاصيل عن القسم :</h5>  " + Categories[cat].description + "</div>");
-
-            };
-            reqCategories.send();
-
             for (var i = 0 ; i < prolist.length ; i++) {
                 if (prolist[i][searchField] == searchVal) {
                     div.insertAdjacentHTML('beforeend', "<div class='col-md-4 pageelement py-2'><div class='card shadow hover border-primary'><img class='img-fluid card-img-top categoryslider-img ' src='" + products[i].image + "' alt='' style='height: 300px; object-fit: cover;' /><a class='card-header btn btn-outline-primary font-weight-bold rounded-0 border-left-0 border-right-0' href='Product.html?i=" + i + "'><div style='overflow: hidden; white-space: nowrap; direction: rtl'>" + products[i].name + "</div></a><div class='card-body w-100 text-primary categoryslider-body' style='position: absolute; top: 0px; background-color: black; display: none; opacity: .95; overflow-y: scroll; max-height: 100%;'><h5 dir='rtl' class='card-title font-weight-bold text-right'>" + products[i].name + "</h5><p class='card-text text-right text-pre' dir='rtl'>" + products[i].description + "</p><p class='card-text text-right' dir='rtl'>السعر : " + products[i].price + "</p></div><a href='https://wa.me/2" + infolist[0].phone + "?text=" + products[i].name + "' class='btn btn-outline-primary card-footer font-weight-bold border-0 w-100' target='_blank'>اطلب الأن</a></div></div>")
