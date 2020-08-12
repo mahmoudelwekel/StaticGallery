e69ebe5604f13f4e4c13da36077bc4dfcc183054
mahmoudelwekel <mahmoudelwekel@ymail.com>
2020-08-12 09:07:50 +0200

    slider

--- StaticGallery/Partners/FillPartners.js
+++ StaticGallery/Partners/FillPartners.js
@@ -8,7 +8,7 @@
     var PartnersDiv = document.getElementById('PartnersDiv');
 
     for (var i = 0 ; i < Partnerslist.length ; i++) {
-        PartnersDiv.insertAdjacentHTML('beforeend', " <a class=' text-decoration-none'  target='_blank' href='" + Partnerslist[i].link + "'> <img class='img-fluid shadow rounded-circle border border-secondary w-75 mx-auto mt-3' src='" + Partnerslist[i].image + "'> <div class='card-body'> <h6 class='card-title font-weight-bolder' style='overflow: hidden; white-space: nowrap; direction: rtl'> " + Partnerslist[i].name + "</h6> </div></a>")
+        PartnersDiv.insertAdjacentHTML('beforeend', " <a class=' text-decoration-none'  target='_blank' href='" + Partnerslist[i].link + "'> <img class='img-fluid shadow rounded-circle border border-secondary w-75 mx-auto mt-3 mb-3 mb-md-0' src='" + Partnerslist[i].image + "'> <div class='card-body d-none d-md-block'> <h6 class='card-title font-weight-bolder' style='overflow: hidden; white-space: nowrap; direction: rtl'> " + Partnerslist[i].name + "</h6> </div></a>")
 
     }
     $('#PartnersDiv').owlCarousel({
@@ -21,10 +21,10 @@
         autoplaySpeed: 1000,
         responsive: {
             0: {
-                items: 2,
+                items: 3,
             },
             600: {
-                items: 4,
+                items: 5,
             },
             1000: {
                 items: 7,
