fd6bf933a98f6002ce51d0b60b0636ba38cb39a6
mahmoudelwekel <mahmoudelwekel@ymail.com>
2020-08-29 13:39:37 +0200

    marsa optimized

--- StaticGallery/Partners/FillPartners.js
+++ StaticGallery/Partners/FillPartners.js
@@ -8,11 +8,12 @@
     var PartnersDiv = document.getElementById('PartnersDiv');
 
     for (var i = 0 ; i < Partnerslist.length ; i++) {
-        PartnersDiv.insertAdjacentHTML('beforeend', " <a class=' text-decoration-none'  target='_blank' href='" + Partnerslist[i].link + "'> <img class='img-fluid shadow rounded-circle border border-secondary w-75 mx-auto mt-3 mb-3 mb-md-0' src='" + Partnerslist[i].image + "'> <div class='card-body d-none d-md-block'> <h6 class='card-title font-weight-bolder' style='overflow: hidden; white-space: nowrap; direction: rtl'> " + Partnerslist[i].name + "</h6> </div></a>")
+        PartnersDiv.insertAdjacentHTML('beforeend', " <a class=' text-decoration-none'  target='_blank' href='" + Partnerslist[i].link + "'> <img class='img-fluid shadow-sm rounded-circle border border-secondary w-75 mx-auto mt-3' src='" + Partnerslist[i].image + "'> <div class='card-body d-none d-md-block'> <h6 class='card-title font-weight-bolder' style='overflow: hidden; white-space: nowrap; direction: rtl'> " + Partnerslist[i].name + "</h6> </div></a>")
 
     }
     $('#PartnersDiv').owlCarousel({
         loop: true,
+        dots: false,
         margin: 0,
         responsiveClass: true,
         autoplay: true,
