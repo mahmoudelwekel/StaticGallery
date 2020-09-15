ee6a31f55f8ba446652d1542e20554b223b9695c
mahmoudelwekel <mahmoudelwekel@ymail.com>
2020-09-15 12:29:19 +0200

    add licenses

---
+++ StaticGallery/Licenses/FillLicenses.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    var div = document.getElementById('pageRow');
+
+    var d = new Date();
+    var nocach = d.getTime();
+
+
+
+    $.getJSON('Licenses/Licenses.json?nocache=' + nocach, function (prolist) {
+
+            for (var i = 0; i < prolist.length; i++) {
+                div.insertAdjacentHTML('beforeend', "<div class='col-12'><div class='card border border-primary shadow text-left mb-3'> <div class='card-body'> <h5 class='card-title font-weight-bolder'>" + prolist[i].name + "</h5> <hr /> <div class='card-text'> <table class='mw-100 table table-sm table-borderless text-primary'> <tbody class='mw-100'> <tr class='mw-100'> <td class='d-flex justify-content-center'><i class='fas fa-hdd fa-lg'></i></td> <td class='mw-100 break'>" + prolist[i].mac + "</td> </tr> <tr class='mw-100'> <td class='d-flex justify-content-center'><i class='fas fa-business-time fa-lg'></i></td> <td class='mw-100 break'>" + prolist[i].start + " ~ " + prolist[i].end + "</td> </tr> <tr class='mw-100'> <td class='d-flex justify-content-center'> <i class='fas fa-user-tie fa-lg '></i> </td> <td class='mw-100 break'>" + prolist[i].org + "</td> </tr> <tr class='mw-100'> <td class='d-flex justify-content-center'> <i class='fas fa-map-marked-alt fa-lg'></i> </td> <td class='mw-100 break'>" + prolist[i].place + "</td> </tr> <tr class='mw-100'> <td class='d-flex justify-content-center'> <i class='fas fa-comment-alt fa-lg'></i> </td> <td class='mw-100 break'>" + prolist[i].comment + "</td> </tr> </tbody> </table> </div> <hr /> <a href='tel:" + prolist[i].phone + "' target='_blank' class='text-decoration-none mr-1'> <i class='fas fa-phone-square fa-2x' style='color: darkslategrey;'></i> </a> <a href='https://wa.me/2" + prolist[i].phone + "' target='_blank' class='text-decoration-none mr-1'> <i class='fab fa-whatsapp-square fa-2x' style='color: #25D366;'></i> </a> <a href='" + prolist[i].link + "' target='_blank' class='text-decoration-none mr-1'> <i class='fas fa-external-link-square-alt fa-2x' style='color: #3b5998;'></i> </a> </div> </div> </div>")
+            }
+        
+
+
+
+        //$(".card").hover(function (event)
+        //{
+        //    $(".categoryslider-body", this).css("height", $(".categoryslider-img", this).height());
+        //    $(".categoryslider-body", this).fadeIn();
+        //});
+
+        //$(".card").mouseleave(function (event)
+        //{
+        //    $(".categoryslider-body", this).fadeOut();
+        //});
+
+        //$('.paginationspages').paginate({
+        //    scope: $('.pageelement'),
+
+        //    // how many items per page
+        //    perPage: 12,
+        //});
+
+    });
+
+
+
+});
+
---
+++ StaticGallery/Login/Login.js
@@ -0,0 +1,67 @@
+﻿$(function () {
+    if (getCookie('isValid') != 'true') {
+
+        var passwordTxt = prompt("Please enter your password:", "");
+        if (passwordTxt == null || passwordTxt == "") {
+            alert('invalid password !');
+            window.location.reload();
+        } else {
+
+            var reqUsers = new XMLHttpRequest();
+
+            reqUsers.open('GET', 'Login/Users.json?nocache=' + (new Date()).getTime());
+            reqUsers.onload = function () {
+                var User = JSON.parse(reqUsers.responseText);
+
+                var results = [];
+                var searchField = "password";
+                var searchVal = passwordTxt;
+                for (var i = 0 ; i < User.length ; i++) {
+                    if (User[i][searchField] == searchVal) {
+                        results.push(User[i]);
+                    }
+                }
+
+                if (results.length == 1) {
+                    alert('Login Successfully !');
+
+                    setCookie('isValid', true, 1);
+                } else {
+                    alert('invalid password !');
+
+                    window.location.reload();
+                }
+
+            };
+            reqUsers.send();
+
+        }
+    }
+
+});
+
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
