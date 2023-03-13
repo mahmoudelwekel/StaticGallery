cd771f3bac97c8dc52a4b5d8cd474c3a7914a7bf
mahmoudelwekel <mahmoudelwekel@ymail.com>
2023-03-13 21:40:44 +0200

    Update Prices

--- StaticGallery/Info/FillInfo.js
+++ StaticGallery/Info/FillInfo.js
@@ -15,31 +15,31 @@ $(document).ready(function () {
     var d = new Date();
     var nocach = '20211005';
 
-$.getJSON('Info/Info.json?nocache=' + nocach, function (json) {
-    var infolist  = json;
+    $.getJSON('Info/Info.json?nocache=' + nocach, function (json) {
+        var infolist = json;
 
-    $('#call').attr('href', 'Tel:' + infolist[0].phone);
-    $('#whats,.whatsLink').attr('href', 'https://wa.me/2' + infolist[0].phone);
-    $('#logo').attr('src', infolist[0].logo);
-    //$('#tablogo').attr('href', infolist[0].logo);
+        $('#call').attr('href', 'Tel:' + infolist[0].phone);
+        $('#whats,.whatsLink').attr('href', 'https://wa.me/2' + infolist[0].phone);
+        $('#logo').attr('src', infolist[0].logo);
+        //$('#tablogo').attr('href', infolist[0].logo);
 
-    $('#face').attr('href', infolist[0].face);
-    $('#messenger').attr('href', infolist[0].messenger);
-    $('#insta').attr('href', infolist[0].insta);
+        $('#face').attr('href', infolist[0].face);
+        $('#messenger').attr('href', infolist[0].messenger);
+        $('#insta').attr('href', infolist[0].insta);
 
-    $('#placename,#brandName').text(infolist[0].name);
-    document.getElementById("titletag").innerText = infolist[0].name + ' - الرئيسية';
+        $('#placename,#brandName').text(infolist[0].name);
+        document.getElementById("titletag").innerText = infolist[0].name + ' - الرئيسية';
 
-    document.getElementById("adsMarquee").innerText = infolist[0].ads;
+        document.getElementById("adsMarquee").innerText = infolist[0].ads;
 
-    document.getElementById("placeinfo").insertAdjacentHTML('beforeend', infolist[0].about);
+        document.getElementById("placeinfo").insertAdjacentHTML('beforeend', infolist[0].about);
 
-    $("link[rel='icon'],link[rel='apple-touch-icon']").attr('href', infolist[0].logo);
-      
-    $("meta[name='twitter:title'],meta[itemprop='name'],meta[property='og:title']").attr('content', infolist[0].name);
-    $("meta[name='twitter:description'],meta[itemprop='description'],meta[property='og:description'],meta[name='description']").attr('content', infolist[0].about);
-    $("meta[name='twitter:image'],meta[itemprop='image'],meta[property='og:image']").attr('content', infolist[0].cover);
+        $("link[rel='icon'],link[rel='apple-touch-icon']").attr('href', infolist[0].logo);
+
+        $("meta[name='twitter:title'],meta[itemprop='name'],meta[property='og:title']").attr('content', infolist[0].name);
+        $("meta[name='twitter:description'],meta[itemprop='description'],meta[property='og:description'],meta[name='description']").attr('content', infolist[0].about);
+        $("meta[name='twitter:image'],meta[itemprop='image'],meta[property='og:image']").attr('content', infolist[0].cover);
 
 
-});
+    });
 });
